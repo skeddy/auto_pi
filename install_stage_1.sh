bce7050705d2c3d7f8a44e47af2345a7d6643858
Rob Sked <skeddy@users.noreply.github.com>
2024-03-14 19:17:46 +0000

    Update install_stage_1.sh

--- install_stage_1.sh
+++ install_stage_1.sh
@@ -6,5 +6,13 @@
 # Then the upgrade
 /usr/bin/sudo apt-get -y upgrade
 
+# Remove Crap
+/usr/bin/sudo apt purge geany bluej greenfoot-unbundled mu-editor scratch* sonic-pi sense-emu-tools thonny smartsim libreoffice claws-mail  openjdk-* libreoffice-* -y
+usr/bin/sudo rm -rf MagPi/
+
+# Clean up
+/usr/bin/sudo apt -y auto-clean
+/usr/bin/sudo apt -y auto-remove
+
 # Finally rpi-update
 /usr/bin/sudo rpi-update
