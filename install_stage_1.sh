3d463fe1c92fa7a5056f7c3cd3a48a630abab971
Rob Sked <skeddy@users.noreply.github.com>
2024-03-14 20:16:58 +0000

    Update install_stage_1.sh

--- install_stage_1.sh
+++ install_stage_1.sh
@@ -22,18 +22,3 @@ echo "All done!"
 echo
 echo "Please reboot if a new firmware has been installed"
 echo
-
-# Reboot
-
-COUNTER=20
-while [ 1 ] 
-do
-    if [ ${COUNTER} -eq 0 ]
-    then
-        break
-    fi
-    echo "System rebooting in ${COUNTER}"
-    sleep 1
-    COUNTER=$( echo "${COUNTER}-1" | bc )
-done
-sudo reboot
