f89feeb7726d877b5555b5bf0071544791ff2e89
Rob Sked <skeddy@users.noreply.github.com>
2025-06-09 12:40:17 +0100

    Adding in VNC install

--- install_stage_1.sh
+++ install_stage_1.sh
@@ -1,4 +1,4 @@
- #!/bin/bash -e
+#!/bin/bash -e
 
 # First get updates
 sudo apt-get update -y
@@ -8,11 +8,18 @@ sudo apt-get full-upgrade -y
 sudo apt dist-upgrade -y
 
 # Remove Crap
-sudo apt purge geany firefox bluej greenfoot-unbundled mu-editor scratch* sonic-pi sense-emu-tools thonny smartsim libreoffice claws-mail  openjdk-* libreoffice-* -y
+sudo apt purge geany firefox bluej greenfoot-unbundled mu-editor scratch* sonic-pi sense-emu-tools thonny smartsim libreoffice claws-mail openjdk-* libreoffice-* -y
 
 # Clean up
 sudo apt -y auto-clean
 sudo apt -y auto-remove
+
+# Install and enable VNC
+echo
+echo "Installing and enabling VNC..."
+sudo apt install realvnc-vnc-server realvnc-vnc-viewer -y
+sudo systemctl enable vncserver-x11-serviced.service
+sudo systemctl start vncserver-x11-serviced.service
 
 # Finally rpi-update
 sudo rpi-update -y
@@ -20,5 +27,6 @@ sudo rpi-update -y
 echo
 echo "All done!"
 echo
-echo "Please reboot if a new firmware has been installed"
+echo "VNC enabled"
+echo "Please reboot if a new firmware has been installed."
 echo
